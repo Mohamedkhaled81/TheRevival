95faa85a2d9db6f2db7415027b63b19e6b1dada4
Mohamed Khaled <mohamed.khaled.200540@gmail.com>
2026-01-07 17:26:11 +0200

    Adding Last-Example using procedural-Programming Depends on Global states

--- Project-3/Ex7/static/myScriptzz.js
+++ Project-3/Ex7/static/myScriptzz.js
@@ -1,13 +1,105 @@
-let rowNum = Number(prompt("Enter the number of rows.. :D"));
+/*
+Procedural Programming but depends on mutating Global values
+as Arrays is not permitted in that task....
+MK :D..
+**/
 
-function drawTriangle(rows) {
-    for(let i = 0;i < rows;i++){
-        let tempStr = "";
-        for(let j = 0;j < i + 1;j++){
-            tempStr += "*";
+let errFlag = false, sum = 0, display = "", strtVal, lstVal, mode;
+
+function validateNumber(value) {
+    return !isNaN(value);
+}
+
+function validateMode(value) {
+    return value == "even" || value == "odd" || value == "no";
+}
+
+function getUserInputs() {
+    strtVal = Number(prompt("Enter the Starting Value.."));
+    lstVal = Number(prompt("Enter the Last Value..")); 
+    mode = prompt("Enter the mode < odd, even, no >..").toLowerCase();
+}
+
+function validateInputData() {
+    do {
+        getUserInputs();
+        errFlag = validateNumber(strtVal) && validateNumber(lstVal) && validateMode(mode);
+        if (!errFlag) console.log('%cError, Please Re-enter the values...', 'color: red; font-weight: bold;');
+    } while(!errFlag)
+}
+
+function handleEven() {
+    if (strtVal <= lstVal) {
+        for(let currVal = strtVal;currVal <= lstVal; currVal++) {
+            if(currVal % 2 == 0) {
+                display += ` ${currVal}`;
+                sum += currVal;
+            }
         }
-        console.log(tempStr);
+    }else {
+        for(let currVal = strtVal;currVal >= lstVal; currVal--) {
+            if(currVal % 2 == 0) {
+                display += ` ${currVal}`;
+                sum += currVal;
+            }
+        }
     }
 }
 
-drawTriangle(rowNum);
+function handleOdd() {
+    if (strtVal <= lstVal) {
+        for(let currVal = strtVal;currVal <= lstVal; currVal++) {
+            if(currVal % 2 != 0) {
+                display += ` ${currVal}`;
+                sum += currVal;
+            }
+        }
+    }else {
+        for(let currVal = strtVal;currVal >= lstVal; currVal--) {
+            if(currVal % 2 != 0) {
+                display += ` ${currVal}`;
+                sum += currVal;
+            }
+        }
+    }
+}
+
+function handleNo() {
+    if (strtVal <= lstVal) {
+        for(let currVal = strtVal;currVal <= lstVal; currVal++) {
+            display += ` ${currVal}`;
+            sum += currVal;
+        }
+    }else {
+        for(let currVal = strtVal;currVal >= lstVal; currVal--) {
+            display += ` ${currVal}`;
+            sum += currVal;
+        }
+    }
+}
+
+function handleMode(){
+    switch (mode) {
+        case "even":
+            handleEven();
+            break;
+        case "odd":
+            handleOdd();
+            break;
+        default:
+            handleNo();
+    }
+}
+
+function displayOutput() {
+    console.log(`%cThe Output will be: ${display}`, `color:green; font-weight: bold;`);
+    console.log(`%cThe Sum will be: ${sum}`, `color:green; font-weight: bold;`);
+}
+
+function runApp() {
+    validateInputData();
+    handleMode();
+    displayOutput();
+}
+
+runApp();
